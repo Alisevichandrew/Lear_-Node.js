8b6753463c69e1230fa8973318779b516112a226
AlisevchAndrew <dopysk@yandex.ru>
2026-03-26 21:43:45 +0300

    Create routes and contacts with data base

---
+++ controllers/contactController.js
@@ -0,0 +1,48 @@
+//bellow description
+//@desc Get all contacts
+//@route GET /api/contacts
+//@access public
+const getContacts = (req, res) => {
+    res.status(200).json({ message: "Get all contacts" });
+};
+
+//bellow description
+//@desc Create New contact
+//@route POST /api/contacts
+//@access public
+const createContact = (req, res) => {
+    res.status(201).json({ message: "Create contact" });
+};
+
+//bellow description
+//@desc  Get contact
+//@route GET /api/contacts/:id
+//@access public
+const getContact = (req, res) => {
+    res.status(200).json({ message: `Get contact for ${req.params.id}` });
+};
+
+//bellow description
+//@desc Update contact
+//@route PUT /api/contacts/:id
+//@access public
+const UpdateContact = (req, res) => {
+    res.status(200).json({ message: `Update contact for ${req.params.id}` });
+};
+
+//bellow description
+//@desc DELETE contact
+//@route DELETE /api/contacts/:id
+//@access public
+const deleteContact = (req, res) => {
+    res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+};
+
+module.exports = {
+    getContacts,
+    createContact,
+    getContact,
+    UpdateContact,
+    deleteContact,
+};
+
