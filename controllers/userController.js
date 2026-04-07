fe74726950b3ed52bbeb01099a7e0557ce06efda
AlisevchAndrew <dopysk@yandex.ru>
2026-04-07 09:54:21 +0300

    Verify JWT Token Middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 //@desc Register a user
@@ -39,13 +40,37 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/Login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Login user" });
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new error("All fields are necessary !");
+    }
+    const user = await User.findOne({ email });
+    // compare password with hash password
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            {
+                user: {
+                    username: user.username,
+                    email: user.email,
+                    id: user.id,
+                },
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "1m" }
+        );
+        res.status(200).json({ accessToken });
+    } else {
+        res.status(401);
+        throw new Error("email or password is not valid");
+    }
 });
+
 //@desc Current user info
 //@route POST /api/users/current
 //@access private
 const currentUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Current user information" });
+    res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
